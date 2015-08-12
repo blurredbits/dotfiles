e8095fa766411e946171862365c825a232c14ae1
Mark Morris <mmorris@hela.local>
2015-08-12 09:42:05 -0600

    First commit for dotfiles

---
+++ makesymlinks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###########################################
+# This script creates symlinks from the 
+# home directory to any desired dotfiles 
+# in ~/dotfiles
+##########################################
+
+# Variables
+
+dir=~/dotfiles           # dotfiles directory
+olddir=~/dotfiles_old    # old dotfiles backup directory
+files="bashprofile gitconfig gitignore pull push railsrc"   # list of files/folder to symlink in homedir
+
+# Create dotfiles_old in homedir
+echo "Create $olddir for back of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...and done!"
+
+# Change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...and done!"
+
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/#file ~/.$file
+done
